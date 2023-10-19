6a32fd6c72cd7e1fe0d02e44b2661be4f3cde86d
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-19 18:29:45 -0400

    modify server.js for new GraphQL queries

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// Import necessary modules
+const express = require('express'); // Express.js for server creation
+const path = require('path'); // Node.js path module for handling and transforming file paths
+const db = require('./config/connection'); // Database connection configuration
+const { ApolloServer } = require('apollo-server-express'); // ApolloServer, GraphQL server for Express.js
+const { typeDefs, resolvers } = require('./schemas'); // GraphQL schema - typeDefs and resolvers
 
+// Initialize Express.js server
 const app = express();
+// Use environment defined port or 3001 if not defined
 const PORT = process.env.PORT || 3001;
 
+// Express.js middleware for parsing request body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Create an instance of ApolloServer
+const server = new ApolloServer({
+  typeDefs, // GraphQL schema to use
+  resolvers, // Resolver functions to handle GraphQL queries
+  context: ({ req }) => { // Context for GraphQL resolvers
+    return {
+      user: req.user, // Pass user info from the request object to the resolvers
+    };
+  },
+});
+
+// Apply Apollo middleware to Express.js server
+server.applyMiddleware({ app });
+
+// In a production environment, serve static assets from the build directory
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
+// Connect to the database and start the server
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
