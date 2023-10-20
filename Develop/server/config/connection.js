41839f45ebb135a1744b50b9d9ec4c094e2a4186
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-20 15:31:57 -0400

    update connection

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://minhmeoquay:SN1PqRsN7ZHCUBoy@cluster0.uzzmayw.mongodb.net/?retryWrites=true&w=majority";
+// Use the MONGODB_URI environment variable if available, otherwise fall back to the hardcoded connection string
+const connectionString = process.env.MONGODB_URI || "mongodb+srv://minhmeoquay:SN1PqRsN7ZHCUBoy@cluster0.uzzmayw.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
