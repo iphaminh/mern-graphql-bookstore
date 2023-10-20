e47c02ec56378af2d1aab7643f9a6fc1e462c2a9
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-20 15:56:05 -0400

    update server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -25,12 +25,12 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
-  }
+}
 
   db.once('open', () => {
     app.listen(PORT, () => {
