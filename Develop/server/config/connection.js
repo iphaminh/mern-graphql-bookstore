807b8007f3c2eec4a6d3673ae61b7c13094b6ecd
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-20 14:47:26 -0400

    update connection

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
+const DB_URI = 'mongodb://localhost:27017/googlebooksDB';
+
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 
 module.exports = mongoose.connection;
