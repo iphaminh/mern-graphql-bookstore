f9068c9a868887784df61611e092f5529396812e
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-19 22:17:30 -0400

    remove syntax

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -4,8 +4,8 @@ import gql from "graphql-tag";
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token // JWT token for authentication when login
-      user { //for client display
+      token 
+      user { 
         _id 
         username 
       }
@@ -17,10 +17,10 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-      token // JWT token for authentication
+      token 
       user {
-        _id // Unique ID of the new user
-        username // Username of the new user
+        _id 
+        username 
       }
     }
   }
@@ -30,10 +30,10 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
   mutation saveBook($book: SavedBookInput!) {
     saveBook(book: $book) {
-      username // Username of the user who saved the book
-      email // Email of the user who saved the book
-      bookCount // Total number of books saved by the user
-      savedBooks { // List of books saved by the user
+      username 
+      email 
+      bookCount 
+      savedBooks { 
         authors
         description
         bookId
@@ -49,10 +49,10 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      username // Username of the user who removed the book
-      email // Email of the user who removed the book
-      bookCount // Updated total number of books saved by the user
-      savedBooks { // Updated list of books saved by the user
+      username 
+      email 
+      bookCount 
+      savedBooks { 
         authors
         description
         bookId
