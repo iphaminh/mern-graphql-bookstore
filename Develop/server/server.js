bef8cf91669303afd7cda00976971b30c618723e
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-19 23:35:15 -0400

    update server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,48 +1,41 @@
-const path = require('path');
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas')
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
+// Import necessary modules
+const express = require('express'); // Express.js for server creation
+const path = require('path'); // Node.js path module for handling and transforming file paths
+const db = require('./config/connection'); // Database connection configuration
+const { ApolloServer } = require('apollo-server-express'); // ApolloServer, GraphQL server for Express.js
+const { typeDefs, resolvers } = require('./schemas'); // GraphQL schema - typeDefs and resolvers
 
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
-
-const { authMiddleware } = require("./utils/auth")
-// const routes = require('./routes');
+// Initialize Express.js server
 const app = express();
+// Use environment defined port or 3001 if not defined
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-});
 
+// Express.js middleware for parsing request body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-// app.use(routes);
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// Create an instance of ApolloServer
+const server = new ApolloServer({
+  typeDefs, // GraphQL schema to use
+  resolvers, // Resolver functions to handle GraphQL queries
+  context: ({ req }) => { // Context for GraphQL resolvers
+    return {
+      user: req.user, // Pass user info from the request object to the resolvers
+    };
+  },
 });
 
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
+// Ensure the server is started before applying middleware
+server.start().then(() => {
+  server.applyMiddleware({ app });
 
-    db.once("open", () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!}`);
-            console.log(
-                `🌍 Now listening on http://localhost:${PORT}${server.graphqlPath}`
-            );
-        });
-    });
-};
+  // In a production environment, serve static assets from the build directory
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-startApolloServer(typeDefs, resolvers);
+  // Connect to the database and start the server
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
+  });
+});
