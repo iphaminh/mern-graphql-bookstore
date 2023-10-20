ec31ef82b3ba66046d97f01de9ba3269dc44f7c8
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-19 22:18:09 -0400

    remove again

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,23 +1,9 @@
 import gql from "graphql-tag";
 
-// Mutation to log in a user and get a token
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token 
-      user { 
-        _id 
-        username 
-      }
-    }
-  }
-`;
-
-// Mutation to add (register) a new user and get a token
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token 
+      token
       user {
         _id 
         username 
@@ -26,14 +12,25 @@ export const ADD_USER = gql`
   }
 `;
 
-// Mutation to save a book to the user's savedBooks list
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const SAVE_BOOK = gql`
   mutation saveBook($book: SavedBookInput!) {
     saveBook(book: $book) {
-      username 
-      email 
-      bookCount 
-      savedBooks { 
+      username
+      email
+      bookCount
+      savedBooks {
         authors
         description
         bookId
@@ -45,14 +42,13 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-// Mutation to remove a book from the user's savedBooks list
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      username 
-      email 
-      bookCount 
-      savedBooks { 
+      username
+      email
+      bookCount
+      savedBooks {
         authors
         description
         bookId
