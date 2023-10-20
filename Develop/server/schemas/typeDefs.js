4f2d7aa1241d12dfbeadf84b91435c787954cd3f
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-19 22:14:48 -0400

    remove commenrs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,18 +1,16 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-# Define the Book type with its fields
 type Book {
     _id: ID
     authors: [String]
     description: String
-    bookId: String        # Book's unique identifier
+    bookId: String
     image: String
     link: String
     title: String
 }
 
-# Define the input type for saving a book
 input BookInput {
     authors: [String]
     description: String
@@ -22,30 +20,26 @@ input BookInput {
     title: String
 }
 
-# Define the User type with its fields
 type User {
-    _id: ID               # Unique ID for the user
+    _id: ID
     username: String
     email: String
     password: String
-    savedBooks: [Book]    # List of books saved by the user
+    savedBooks: [Book]
 }
 
-# Define the Auth type for authentication responses
 type Auth {
-    token: ID!            # Authentication token
-    user: User            # User data
+    token: ID!
+    user: User
 }
 
-# Define available queries
 type Query {
-    me: User              # Fetch the current user's data
+    me: User
 }
 
-# Define available mutations (actions)
 type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username:String!, email: String!, password:String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: BookInput!): User
     removeBook(bookId: ID!): User
 }
